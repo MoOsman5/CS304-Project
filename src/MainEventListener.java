1fea29bef2b9cc6845977b19ad76bc40e7ccd2b5
MoOsman <92763481+MoOsman5@users.noreply.github.com>
2024-08-19 11:00:17 +0300

    Update MainEventListener.java

--- src/MainEventListener.java
+++ src/MainEventListener.java
@@ -1,2 +1,43 @@
-public class MainEventListener {
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+
+public class MainEventListener implements GLEventListener {
+    
+    @Override
+    public void init(GLAutoDrawable glAutoDrawable) {
+        GL gl = glAutoDrawable.getGL();
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        gl.glLineWidth(2.0f);
+        gl.glMatrixMode(GL.GL_PROJECTION);
+        gl.glLoadIdentity();
+        gl.glOrtho(-250.0, 250.0, -150.0, 150.0, -1, 1);
+    }
+
+    @Override
+    public void display(GLAutoDrawable glAutoDrawable) {
+        GL gl = glAutoDrawable.getGL();
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+
+    }
+
+
+
+    @Override
+    public void reshape(GLAutoDrawable glAutoDrawable, int x, int y, int width, int height) {
+        GL gl = glAutoDrawable.getGL();
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL.GL_PROJECTION);
+        gl.glLoadIdentity();
+        gl.glOrtho(-250.0, 250.0, -150.0, 150.0, -1, 1);
+        gl.glMatrixMode(GL.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+
+    @Override
+    public void displayChanged(GLAutoDrawable glAutoDrawable, boolean modeChanged, boolean deviceChanged) {
+        // Handle display changes if needed
+    }
+    
 }
+
