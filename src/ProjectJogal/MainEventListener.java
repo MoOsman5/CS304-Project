a5c49ff78b23f4dba0ddd1dd57e508df92a91877
youssefrafat257 <146446248+youssefrafat257@users.noreply.github.com>
2024-08-21 17:55:27 +0200

    move of basket

--- src/ProjectJogal/MainEventListener.java
+++ src/ProjectJogal/MainEventListener.java
@@ -9,108 +9,75 @@ import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
 
-public class MainEventListener implements GLEventListener,KeyListener {
-    int chickenIndex = 0;  
+public class MainEventListener implements GLEventListener, KeyListener {
+    int chickenIndex = 0;
     int RepeatCounter = 0;
     int maxWidth = 100;
     int maxHeight = 100;
-    int[] x = {5,25,45, 65,85};
-    int[] y = {70, 69, 69,70,70};
-    int BasketX=0;
-    int BasketY=15;
+    int[] x = {5, 25, 45, 65, 85};
+    int[] y = {70, 69, 69, 70, 70};
+    int BasketX = 50;  // في منتصف الشاشة
+    int BasketY = 15;
+    int basketWidth = 10; // حجم السلة
     String assetsFolderName = "Assets";
-    String textureNames[] = {"Chicken1.png","Chicken2.png","Basket.png","Treebranch.png","Egg1.png","Egg2.png","Egg3.png","Health.png","Background2.png"};
+    String textureNames[] = {"Chicken1.png", "Chicken2.png", "Basket.png", "Treebranch.png", "Egg1.png", "Egg2.png", "Egg3.png", "Health.png", "Background2.png"};
     TextureReader.Texture texture[] = new TextureReader.Texture[textureNames.length];
     int textures[] = new int[textureNames.length];
-    int BasketIndex = 2 ;
-    
-    
+    int BasketIndex = 2;
+
+   
     public void init(GLAutoDrawable glAutoDrawable) {
-         GL gl = glAutoDrawable.getGL();
-        gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);    //This Will Clear The Background Color To Black
-        
-        gl.glEnable(GL.GL_TEXTURE_2D);  // Enable Texture Mapping
-        gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);	
+        GL gl = glAutoDrawable.getGL();
+        gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);    // خلفية بيضاء
+
+        gl.glEnable(GL.GL_TEXTURE_2D);  // تمكين استخدام القوام
+        gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
         gl.glGenTextures(textureNames.length, textures, 0);
-        
-        for(int i = 0; i < textureNames.length; i++){
+
+        for (int i = 0; i < textureNames.length; i++) {
             try {
-                texture[i] = TextureReader.readTexture(assetsFolderName + "//" + textureNames[i] , true);
+                texture[i] = TextureReader.readTexture(assetsFolderName + "//" + textureNames[i], true);
                 gl.glBindTexture(GL.GL_TEXTURE_2D, textures[i]);
 
-//                mipmapsFromPNG(gl, new GLU(), texture[i]);
                 new GLU().gluBuild2DMipmaps(
-                    GL.GL_TEXTURE_2D,
-                    GL.GL_RGBA, // Internal Texel Format,
-                    texture[i].getWidth(), texture[i].getHeight(),
-                    GL.GL_RGBA, // External format from image,
-                    GL.GL_UNSIGNED_BYTE,
-                    texture[i].getPixels() // Imagedata
-                    );
-            } catch( IOException e ) {
-              System.out.println(e);
-              e.printStackTrace();
+                        GL.GL_TEXTURE_2D,
+                        GL.GL_RGBA, // Internal Texel Format,
+                        texture[i].getWidth(), texture[i].getHeight(),
+                        GL.GL_RGBA, // External format from image,
+                        GL.GL_UNSIGNED_BYTE,
+                        texture[i].getPixels() // Imagedata
+                );
+            } catch (IOException e) {
+                System.out.println(e);
+                e.printStackTrace();
             }
         }
     }
 
     @Override
-    
     public void display(GLAutoDrawable glAutoDrawable) {
-           GL gl = glAutoDrawable.getGL();
-             gl.glClear(GL.GL_COLOR_BUFFER_BIT);       
-            gl.glLoadIdentity(); 
-            DrawBackground(gl);
-       
-       //chicken
-       RepeatCounter++;
+        GL gl = glAutoDrawable.getGL();
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+        gl.glLoadIdentity();
+        DrawBackground(gl);
+
+        // move of chicken
+        RepeatCounter++;
         if (RepeatCounter >= 5) {
             chickenIndex++;
             chickenIndex = chickenIndex % 2;
             RepeatCounter = 0;
-          
         }
-         
-         for (int i = 0; i < 4; i++) {
-            DrawSprite(gl, x[i], y[i],chickenIndex , 2, 0);   
-        }
-          //Treebranch
-    DrawTreebranch(gl,0, 61, 3, 1.0, 0);
 
-         
-         
-         //basket
-           DrawSprite(gl, BasketX, BasketY, BasketIndex, 2, 0);
-          
-   }
-public double sqrdDistance(int x, int y, int x1, int y1){
-        return Math.pow(x-x1,2)+Math.pow(y-y1,2);
+        // chicken 
+        for (int i = 0; i < 5; i++) {
+            DrawSprite(gl, x[i], y[i], chickenIndex, 2, 0);
+        }
+
+        // basket
+        DrawSprite(gl, BasketX, BasketY, BasketIndex, 2, 0);
+
     }
-public void DrawTreebranch(GL gl,int x, int y, int index, double scale, double angle) {
-    gl.glEnable(GL.GL_BLEND);
-    gl.glBindTexture(GL.GL_TEXTURE_2D, textures[index]);
-
-    gl.glPushMatrix();
-    gl.glTranslated(x / (maxWidth / 2.0) - 0.9, y / (maxHeight / 2.0) - 0.9, 0);
-    gl.glScaled(2.0 * scale, 0.1 * scale, 1); // Scaled 2.0 to cover full width
-    gl.glRotated(angle, 0, 0, 1);
-
-    gl.glBegin(GL.GL_QUADS);
-    gl.glTexCoord2f(0.0f, 0.0f);
-    gl.glVertex3f(-1.0f, -1.0f, -1.0f);
-    gl.glTexCoord2f(1.0f, 0.0f);
-    gl.glVertex3f(1.0f, -1.0f, -1.0f);
-    gl.glTexCoord2f(1.0f, 1.0f);
-    gl.glVertex3f(1.0f, 1.0f, -1.0f);
-    gl.glTexCoord2f(0.0f, 1.0f);
-    gl.glVertex3f(-1.0f, 1.0f, -1.0f);
-    gl.glEnd();
-    gl.glPopMatrix();
-
-    gl.glDisable(GL.GL_BLEND);
-}
-
-
 
     public void DrawSprite(GL gl, int x, int y, int index, double scale, double angle) {
         gl.glEnable(GL.GL_BLEND);
@@ -135,52 +102,56 @@ public void DrawTreebranch(GL gl,int x, int y, int index, double scale, double a
 
         gl.glDisable(GL.GL_BLEND);
     }
-   
 
-    public void DrawBackground(GL gl){
-        gl.glEnable(GL.GL_BLEND);	
-        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[textures.length-1]);	// Turn Blending On
+    public void DrawBackground(GL gl) {
+        gl.glEnable(GL.GL_BLEND);
+        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[textures.length - 1]);
 
         gl.glPushMatrix();
-            gl.glBegin(GL.GL_QUADS);
-            // Front Face
-                gl.glTexCoord2f(0.0f, 0.0f);
-                gl.glVertex3f(-1.0f, -1.0f, -1.0f);
-                gl.glTexCoord2f(1.0f, 0.0f);
-                gl.glVertex3f(1.0f, -1.0f, -1.0f);
-                gl.glTexCoord2f(1.0f, 1.0f);
-                gl.glVertex3f(1.0f, 1.0f, -1.0f);
-                gl.glTexCoord2f(0.0f, 1.0f);
-                gl.glVertex3f(-1.0f, 1.0f, -1.0f);
-            gl.glEnd();
+        gl.glBegin(GL.GL_QUADS);
+        gl.glTexCoord2f(0.0f, 0.0f);
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+        gl.glTexCoord2f(1.0f, 0.0f);
+        gl.glVertex3f(1.0f, -1.0f, -1.0f);
+        gl.glTexCoord2f(1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+        gl.glTexCoord2f(0.0f, 1.0f);
+        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+        gl.glEnd();
         gl.glPopMatrix();
-        
+
         gl.glDisable(GL.GL_BLEND);
     }
 
     @Override
     public void reshape(GLAutoDrawable glAutoDrawable, int x, int y, int width, int height) {
-      
     }
 
     @Override
     public void displayChanged(GLAutoDrawable glAutoDrawable, boolean modeChanged, boolean deviceChanged) {
-        // Handle display changes if needed
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_LEFT) {
+            BasketX -= 5;
+            if (BasketX < 0) {
+                BasketX = 0; 
+            }
+        } else if (key == KeyEvent.VK_RIGHT) {
+            BasketX += 5;
+            if (BasketX > maxWidth - basketWidth) {
+                BasketX = maxWidth - basketWidth; 
+            }
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
 }
