25f705af26e7dd80cd8f13ed006826121ddac983
mina magdy <120422463+mina20028@users.noreply.github.com>
2024-08-21 17:12:43 +0200

    Eggclass

---
+++ src/ProjectJogal/Egg.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjectJogal;
+
+/**
+ *
+ * @author mena
+ */
+public class Egg {
+    int x ;
+    int y;
+    int index;
+    
+     Egg(int x, int y, int index) {
+            this.x = x;
+            this.y = y;
+            this.index = index;
+     }
+    
+}
