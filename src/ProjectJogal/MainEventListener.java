2722e8553312015ad9fc95bba5fd7bc780b1c8d5
mina magdy <120422463+mina20028@users.noreply.github.com>
2024-08-22 11:09:54 +0200

    push

--- src/ProjectJogal/MainEventListener.java
+++ src/ProjectJogal/MainEventListener.java
@@ -11,6 +11,11 @@ import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class MainEventListener implements GLEventListener, KeyListener {
     int chickenIndex = 0;  
@@ -19,12 +24,11 @@ public class MainEventListener implements GLEventListener, KeyListener {
     int maxWidth = 100;
     int maxHeight = 100;
     int[] x = {5, 25, 45, 65, 85};
-    int[] y = {70, 70, 70, 70, 70};
-    
+    int[] y = {70, 69, 69, 70, 70};
     int CountNum = 0;
     int Speed = 100; 
     int BasketX = 50;  
-    int BasketY = 5;
+    int BasketY = 15;
     int basketWidth = 10;
      int health = 3;  
     int heartIndex = 7; 
@@ -35,7 +39,9 @@ public class MainEventListener implements GLEventListener, KeyListener {
     int BasketIndex = 2;
     private List<Egg> Eggs = new ArrayList<>();
      List<Egg> Remove = new ArrayList<>();
-    
+     private Clip backgroundMusic; 
+     private Clip gameOverMusic;
+
     public void init(GLAutoDrawable glAutoDrawable) {
         GL gl = glAutoDrawable.getGL();
         gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // This will clear the background color to white
@@ -61,6 +67,16 @@ public class MainEventListener implements GLEventListener, KeyListener {
                 e.printStackTrace();
             }
         }
+        
+            try {
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(getClass().getResource("/" + "Sound" + "/gg.wav"));
+        backgroundMusic = AudioSystem.getClip();
+        backgroundMusic.open(audioStream);
+        backgroundMusic.loop(Clip.LOOP_CONTINUOUSLY); 
+    } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+        e.printStackTrace();
+    }
+
     }
 
     @Override
@@ -69,17 +85,26 @@ public class MainEventListener implements GLEventListener, KeyListener {
         gl.glClear(GL.GL_COLOR_BUFFER_BIT);       
         gl.glLoadIdentity(); 
         DrawBackground(gl);
-          handleKeyPress();
+        if(health>0){
+         handleKeyPress();
         if(Eggs.isEmpty()){
       addEgg();
-      
         }
-         
+        // Chicken
+        RepeatCounter++;
+        if (RepeatCounter >= 5) {
+            chickenIndex++;
+            chickenIndex = chickenIndex % 2;
+            RepeatCounter = 0;
+        }
         
-        if(health>0){
-       
-       
+        // Draw chicken
+        for (int i = 0; i < NumberChicken; i++) {
+            DrawSprite(gl, x[i], y[i], chickenIndex, 2, 0);   
+        }
         
+        // Draw tree branch
+        DrawTreebranch(gl, 0, 61, 3, 1.0, 0);
     
         // Draw eggs
         for (int i = 0; i < Eggs.size(); i++) {
@@ -88,21 +113,12 @@ public class MainEventListener implements GLEventListener, KeyListener {
             egg.y--;
             
                 if (egg.y < 0) {
-                    Speed-=70;
+                    Speed-=5;
                    Remove.add(egg);
-                    health--; 
+                   health--;
                 }
-                    
-               
-                  double dist = sqrdDistance(egg.x,egg.y,BasketX,BasketY);
-                  double radii = Math.pow(2*0.03*maxHeight,2);
-                    boolean isCollided = dist<=radii;
-        System.out.println(isCollided + ", "+ dist + ", "+ radii);
-//        
-         if(isCollided==true){
-                   Remove.add(egg);
-                }
-//               
+                
+                
         }
         
         Eggs.removeAll(Remove);
@@ -113,61 +129,55 @@ public class MainEventListener implements GLEventListener, KeyListener {
             addEgg();
             CountNum = 0;
         }
-        
-         // System.out.println(CountNum+"   "+ Speed);
+          System.out.println(CountNum+"   "+ Speed);
   
      
         DrawSprite(gl, BasketX, BasketY, BasketIndex, 2, 0);
          DrawHealth(gl);
-         
-      
-                 // Chicken
-        RepeatCounter++;
-        if (RepeatCounter >= 5) {
-            chickenIndex++;
-            chickenIndex = chickenIndex % 2;
-            RepeatCounter = 0;
-        }
+            }else {
         
-         //Draw chicken
-        for (int i = 0; i < NumberChicken; i++) {
-            DrawSprite(gl, x[i], y[i], chickenIndex, 2, 0);   
-        }
-        
-        // Draw tree branch
-        DrawTreebranch(gl, 0, 61, 3, 1.0, 0);
-         
-         
-        }
-        else {
          DrawGameOver(gl);
+         stopBackgroundMusic();
         }
-        
     }
     
-    
+    public void stopBackgroundMusic() {
+    if (backgroundMusic != null && backgroundMusic.isRunning()) {
+        backgroundMusic.stop();
+    }
+}
+
     public void DrawHealth(GL gl) {
         for (int i = 0; i < health; i++) {
             DrawSprite(gl, 5 + i * 10, 90, heartIndex, 1, 0); 
         }
     }
-   
 public void DrawGameOver(GL gl) {
-    int gameOverIndex = 8; 
-    DrawSprite(gl, 40, 50, gameOverIndex, 5, 0); 
+    int gameOverIndex = 8;
+    DrawSprite(gl, 40, 50, gameOverIndex, 5, 0);
+    
+    // تشغيل أغنية Game Over
+    stopBackgroundMusic();
+    if (gameOverMusic == null) {
+        try {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(getClass().getResource("/" + "Sound" + "/start.wav"));
+            gameOverMusic = AudioSystem.getClip();
+            gameOverMusic.open(audioStream);
+            gameOverMusic.start(); 
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
 }
-
 
     public double sqrdDistance(int x, int y, int x1, int y1) {
         return Math.pow(x - x1, 2) + Math.pow(y - y1, 2);
     }
 
-    
     public void addEgg() {
         int characterIndex = (int) (Math.random() * NumberChicken);
         int EggX = x[characterIndex];
         int EggY = y[characterIndex];
-                //y[characterIndex];
         int index = (int) (Math.random() * 3) + 4;
         Eggs.add(new Egg(EggX, EggY, index));
     }
@@ -265,17 +275,6 @@ public void DrawGameOver(GL gl) {
                 BasketX = maxWidth - basketWidth; 
             }
         }
-//        if (isKeyPressed(KeyEvent.VK_DOWN)) {
-//            if (BasketY > 0) {
-//                BasketY--;
-//            }
-//      
-//        }
-//        if (isKeyPressed(KeyEvent.VK_UP)) {
-//            if (BasketY < maxHeight-10) {
-//            BasketY++;
-//            }
-//        }
         
     }
 
